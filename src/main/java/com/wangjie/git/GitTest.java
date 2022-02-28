e4917ab1e6a069e66b8f0f753eb42011042c8d19
chase <1661709651@qq.com>
2022-02-28 14:25:54 +0800

    first commit

---
+++ src/main/java/com/wangjie/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.wangjie.git;
+
+/**
+ * Createrd by huaimu
+ * on 2022/2/28
+ * 14:23
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
